94d67a9122981c1b726805e8b2fb2f483ac5a2da
Will-Hellinger <will.hellinger@gmail.com>
2024-11-20 11:36:17 -0500

    Removed Unnecessary function (unoptimized)

--- src/data_manager.py
+++ src/data_manager.py
@@ -1,22 +1,5 @@
+import os
 import json
-
-
-def check_exists(file_path: str | None) -> bool:
-    """
-    Check if the file exists.
-
-    :param file_path: The path to the file.
-    :return: True if the file exists, False otherwise.
-    """
-    
-    if not file_path:
-        return False
-
-    try:
-        with open(file_path, 'r') as f:
-            return True
-    except FileNotFoundError:
-        return False
 
 
 def load_json(file_path: str | None) -> dict | None:
@@ -27,7 +10,7 @@ def load_json(file_path: str | None) -> dict | None:
     :return: The JSON file as a dictionary.
     """
 
-    if check_exists(file_path):
+    if os.path.exists(file_path):
         with open(file_path, 'r') as f:
             return json.load(f)
         
